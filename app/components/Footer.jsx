5998898b1a965354312d216f2ddfa3455c9a9189
vichesa <vichesay@gmail.com>
2023-10-27 17:03:55 +0700

    add footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="py-10 text-center border-t border-[#33353F]">
+      All rights reserved.
+    </div>
+  );
+};
+
+export default Footer;
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
         <div className="w-[90%] lg:max-w-6xl mx-auto my-28 p-4 h-screen items-center">
           {children}
         </div>
+        <Footer />
       </body>
     </html>
   );
